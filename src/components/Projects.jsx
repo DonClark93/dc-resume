07b47e396c36d8e111cde751398d8a22ce706522
Don Clark <123608546+DonClark93@users.noreply.github.com>
2024-12-10 19:10:03 -0500

    filled out

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,19 @@
 export default function Projects(){
     return(
-        <>Projects</>
+        <>
+        <h1>Projects</h1>
+        <h3>What I can talk about</h3>
+        <ul>
+            <li>one</li>
+            <li>two</li>
+            <li>three</li>
+        </ul>
+        <h3>What I can Show</h3>
+        <ul>
+            <li>one</li>
+            <li>two</li>
+            <li>three</li>
+        </ul>
+        </>
     )
 }
--- src/components/WorkHistory.jsx
+++ src/components/WorkHistory.jsx
@@ -5,19 +5,52 @@ export default function WorkHistory() {
             <h2>Application Analyst</h2>
             <h3>2019-2021</h3>
             <p>
-            As an Application Analyst, I was responsible for maintaining and updating demographic information for both internal and external healthcare providers, which is crucial for medical and outreach activities across the organization.  I manage the Netpresenter application and its servers, overseeing all screensaver and digital signage requirements for our six delivery networks, including remote offices. My role involved collaborating with a diverse group of users, up to director level, to design and implement SharePoint sites and webpages that cater to different departmental needs like team collaboration and data analysis.  
+            As an Application Analyst, I was responsible for maintaining and updating 
+            demographic information for both internal and external healthcare providers, 
+            which is crucial for medical and outreach activities across the organization.  
+            I manage the Netpresenter application and its servers, overseeing all screensaver 
+            and digital signage requirements for our six delivery networks, including remote offices. 
+            My role involved collaborating with a diverse group of users, up to director level, 
+            to design and implement SharePoint sites and webpages that cater to different departmental 
+            needs like team collaboration and data analysis.  
             </p>
             <br/>
             <h2>Senior Service Desk Analyst</h2>
             <h3>2016-2019</h3>
             <p>
-            During my tenure as a Senior Service Desk Analyst at YNHH, I provided First and Second Tier support to approximately 30,000 users, including vendors and medical community practices. My responsibilities included ensuring that Service Level Agreements (SLAs) for incident tickets and service requests were met promptly. I troubleshooted issues both on-site and remotely using tools like Bomgar, Remote Desktop Connection, and SCCM. On an average day, I managed 40-50 calls.  I was also in charge of the Service Desk mailbox, which received over 200 emails daily, converting these into actionable tickets and service requests as needed. I played a crucial role in the Critical Incident Response Team, addressing high-severity issues like network outages or unexpected application downtimes. My tasks extended to managing user permissions in Active Directory, updating Organizational Units (OUs) for effective group policy application. Additionally, I structured and conducted training sessions for new employees on various workflows, Epic modules, and ticket assignment processes, all while adhering strictly to HIPAA guidelines concerning Protected Health Information (PHI) and Personal Identifiable Information (PII).
+            During my tenure as a Senior Service Desk Analyst at YNHH, I provided 
+            First and Second Tier support to approximately 30,000 users, including 
+            vendors and medical community practices. My responsibilities included 
+            ensuring that Service Level Agreements (SLAs) for incident tickets and 
+            service requests were met promptly. I troubleshooted issues both on-site 
+            and remotely using tools like Bomgar, Remote Desktop Connection, and 
+            SCCM. On an average day, I managed 40-50 calls.  I was also in charge 
+            of the Service Desk mailbox, which received over 200 emails daily, converting 
+            these into actionable tickets and service requests as needed. I played a 
+            crucial role in the Critical Incident Response Team, addressing high-severity 
+            issues like network outages or unexpected application downtimes. My tasks 
+            extended to managing user permissions in Active Directory, updating 
+            Organizational Units (OUs) for effective group policy application. Additionally, 
+            I structured and conducted training sessions for new employees on various workflows, 
+            Epic modules, and ticket assignment processes, all while adhering strictly to HIPAA 
+            guidelines concerning Protected Health Information (PHI) and Personal Identifiable 
+            Information (PII).
             </p>
             <br/>
             <h1>RICOH</h1>
             <h3>2014-2016</h3>
             <p>
-            From October 2014 to March 2016, I served as a Copier Repair Technician for Ricoh, managing the Northern Westchester territory. My responsibilities included servicing Multi-Function Printers, scanners, and faxes across both private and public sectors, working in diverse settings ranging from factories and small offices/home offices to major business headquarters. As the Lead Technician, I spearheaded a significant installation project at MasterCard International Headquarters in Purchase, NY, managing the setup of over 80 Multi-Function Printers. I utilized technical manuals to diagnose and resolve both mechanical and electrical issues with Ricoh products. My routine duties also involved rebuilding and replacing printer components, upgrading PCB boards, and handling expansion cards for networking and interfacing enhancements.
+            From October 2014 to March 2016, I served as a Copier Repair Technician 
+            for Ricoh, managing the Northern Westchester territory. My responsibilities 
+            included servicing Multi-Function Printers, scanners, and faxes across both 
+            private and public sectors, working in diverse settings ranging from factories 
+            and small offices/home offices to major business headquarters. As the Lead Technician, 
+            I spearheaded a significant installation project at MasterCard International 
+            Headquarters in Purchase, NY, managing the setup of over 80 Multi-Function 
+            Printers. I utilized technical manuals to diagnose and resolve both mechanical 
+            and electrical issues with Ricoh products. My routine duties also involved 
+            rebuilding and replacing printer components, upgrading PCB boards, and 
+            handling expansion cards for networking and interfacing enhancements.
             </p>
         </>
     )
