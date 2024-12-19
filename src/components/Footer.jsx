ca1a24209b2507d85f38a366c49fb3e4141602a2
Donald Clark <donald.clark@ynhh.org>
2024-12-19 08:18:02 -0500

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer(){
     return(
-        <div className="sticky bottom-0 text-gunmetal-700  bg-gunmetal-400">
+        <div className="fixed bottom-0 w-screen text-gunmetal-700  bg-gunmetal-400">
             <div className="flex justify-evenly">
             <div>Testing</div>
             <a href="https://github.com/DonClark93" target="_blank">My Github</a>
