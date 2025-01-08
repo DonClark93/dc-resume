2913eda2e9bc362b3e24fbe56566dd2f333541e7
Don Clark <123608546+DonClark93@users.noreply.github.com>
2025-01-08 17:21:49 -0500

    moved around app/home

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,14 @@
-import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './index.css'
-import Container from './layouts/Container.jsx'
-import AboutMe from './components/AboutMe.jsx'
-import Projects from './components/Projects.jsx'
-import WorkHistory from './components/WorkHistory.jsx'
-import Default from './components/Default.jsx'
-import ResumeButton from "./components/ResumeButton.jsx"
-import Header from "./components/Header.jsx"
-import Footer from "./components/Footer.jsx"
+import Home from './components/Home'
+import { Routes, Route } from 'react-router'
 
 function App() {
-
-  const [activity,setActivity] = useState(Default)
-  
-  const about = <AboutMe/>
-  const projects = <Projects/>
-  const work = <WorkHistory/>
-  
-  function updateView(val){
-    if(val == "about"){
-      setActivity(about)
-    }
-    if(val == "work"){
-      setActivity(work)
-    }
-    if(val == "projects"){
-      setActivity(projects)
-    }
-  }
-
-
   return (
-    <div className="min-h-screen bg-gunmetal-400">
-      <Header/>
-      <br/>
-      <br/>
-      <div className="flex text-4xl justify-around">
-        <ResumeButton updateView={updateView} text={"Projects"} activity={"projects"}/>
-        <ResumeButton updateView={updateView} text={"Work History"} activity={"work"}/>
-        <ResumeButton updateView={updateView} text={"About Me"} activity={"about"}/>
-      </div>
-      <br/>
-      <br/>
-      <Container >
-        {activity}
-      </Container>
-      <br/>
-      <br/>
-      <Footer className="object-center absolute bottom-0 right-1/2 " />
-    </div>
+    <Routes>
+      <Route index element={<Home />} />
+    </Routes>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+import Container from '../layouts/Container'
+import AboutMe from './AboutMe.jsx'
+import Projects from './Projects.jsx'
+import WorkHistory from './WorkHistory.jsx'
+import Default from './Default.jsx'
+import ResumeButton from "./ResumeButton.jsx"
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+
+export default function Home () {
+    const [activity,setActivity] = useState(Default)
+  
+    const about = <AboutMe/>
+    const projects = <Projects/>
+    const work = <WorkHistory/>
+  
+    function updateView(val){
+        if(val == "about"){
+        setActivity(about)
+        }
+        if(val == "work"){
+        setActivity(work)
+        }
+        if(val == "projects"){
+        setActivity(projects)
+        }
+    }
+    
+    return(
+        <div className="min-h-screen bg-gunmetal-400">
+              <Header/>
+              <br/>
+              <br/>
+              <div className="flex text-4xl justify-around">
+                <ResumeButton updateView={updateView} text={"Projects"} activity={"projects"}/>
+                <ResumeButton updateView={updateView} text={"Work History"} activity={"work"}/>
+                <ResumeButton updateView={updateView} text={"About Me"} activity={"about"}/>
+              </div>
+              <br/>
+              <br/>
+              <Container >
+                {activity}
+              </Container>
+              <br/>
+              <br/>
+              <Footer className="object-center absolute bottom-0 right-1/2 " />
+            </div>
+    )
+}
